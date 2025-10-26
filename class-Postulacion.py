75c27aa3adb1acd1beb8ab7060919bfad26ee993
Jorge5084 <karpitejorge@gmail.com>
2025-10-26 14:31:51 -0500

    Actualización de metodos

--- class-Postulacion.py
+++ class-Postulacion.py
@@ -1,20 +1,24 @@
 class Postulacion:
-    def __init__(self,fecha_postulacion,estado,calificacion_obtenida,prioridad):
+    def __init__(self,fecha_postulacion,calificacion_obtenida,estado,prioridad):
+        print(f"Se ha postulado en la fecha {fecha_postulacion} con una calificacion de {calificacion_obtenida}, estado {estado}, con prioridad {prioridad}")
         self.fecha_postulacion=fecha_postulacion
         self.estado=estado
         self.calificacion_obtenida=calificacion_obtenida
         self.prioridad=prioridad
 
     def escoger_carrera(self,carrera):
-        print(f"Ingrese la caerra a la que desea postular: ",carrera)
+        print("Ingrese la caerra a la que desea postular: ",carrera)
    
-    def confirmar_seleccion(self,seleccion):
-        print("Esta seguro? ",seleccion)
+    def confirmar_seleccion(self,):
+        print("Esta seguro de que desea postular a esa carrea? ")
     
-    def cancelar_seleccion(self):
-        print("Seleccion cancelada")
+    def cancelar_seleccion(self,seleccion):
+        if seleccion == "No":
+            print(f"{seleccion}, seleccion cancelada")
+        else:
+            print(f"{seleccion}, seleccion confirmada")
     
-simulacion1 =  Postulacion("5-Octubre", "En proceso","500","Ninguna")
+simulacion1 =  Postulacion("5-Octubre", "500","En proceso","Ninguna")
 simulacion1.escoger_carrera("Arquitectura")
-simulacion1.confirmar_seleccion("Si")
-simulacion1.cancelar_seleccion()
+simulacion1.confirmar_seleccion()
+simulacion1.cancelar_seleccion(input())
