ebc93c38e0fe231cba536e53e25fe09a933a136b
Jorge5084 <karpitejorge@gmail.com>
2025-10-26 14:58:47 -0500

    Actualización de mi--

--- class-PeriodoAcademico.py
+++ class-PeriodoAcademico.py
@@ -16,4 +16,4 @@ class Periodo_Academico:
 Periodo =  Periodo_Academico("15-Septiemnbre", "7-Febrero","Inicidado")
 Periodo.abrir_periodo(input("Ingrese el periodo a iniciar: "))
 Periodo.cerrar_periodo()
-Periodo.consultar_periodo(input())
+Periodo.consultar_periodo()
