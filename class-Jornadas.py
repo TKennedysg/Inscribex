1b193d164ee5478ad698b966644c4e048a0da1ba
tyronealan <alananchundia27@gmail.com>
2025-10-26 13:44:32 -0500

    Actualizacion de la clase Jornada

--- class-Jornadas.py
+++ class-Jornadas.py
@@ -1,5 +1,27 @@
 class Jornada():
-    def __init__(self,fecha,hora,modalidad):
-        self.fecha = fecha
+    def __init__(self,hora,modalidad=""):
         self.hora = hora
         self.modalidad = modalidad
+
+    def asignar(self):
+        print("Se ha asignado la jornada")
+   
+    def modificar(self):
+        print("Jornada se ha modificado")
+   
+    def consultar(self):
+        print(f"Modalidad: {self.modalidad}")
+
+        if self.hora >=7 and self.hora <=11:
+            print("jornada matutina")
+        elif self.hora >=12 and self.hora <=17:
+            print("Jornada vespertina")
+        elif self.hora >=18 and self.hora <=22:
+            print("Jornada nocturna")
+        else:
+            print("la hora no perteneces a ninguna jornada")
+
+
+
+J1 = Jornada(20,"virtual")
+J1.consultar()
