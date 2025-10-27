4e60c43fca9841a5db3b9858d66c0fc09e9f1528
tyronealan <alananchundia27@gmail.com>
2025-10-27 09:00:10 -0500

    Polimorfismo en clase abstracta

--- class-Aspirante.py
+++ class-Aspirante.py
@@ -1,10 +1,21 @@
-class aspirante:
+from abc import ABC, abstractmethod
+
+class Usuario(ABC):
+    def __init__(self, nombre, cedula, correo):
+        self.nombre = nombre
+        self.cedula = cedula 
+        self.correo = correo
+    
+    @abstractmethod
+    def Registrarse(self):
+        pass
+
+class Aspirante(Usuario):
     def __init__(self,nombre,cedula,correo,telefono,direccion):
-        self.nombre=nombre
-        self.cedula=cedula
-        self.correo=correo
+        super().__init__(nombre, cedula, correo)
         self.telefono=telefono
         self.direccion=direccion
+
     def Registrarse(self):
         print("Aspirante Registrado")
         print("Nombre: ",self.nombre)
@@ -12,16 +23,34 @@ class aspirante:
         print("Correo: ",self.correo)
         print("Telefono: ",self.telefono)
         print("Direccion: ",self.direccion)
+
     def postularse(self):
         print("Aspirante Postulado")
         print("Nombre: ",self.nombre)
         print("Cedula: ",self.cedula)
+
     def consultarEstado(self):
         print("Consultando estado de Aspirante")
         print("Nombre: ",self.nombre)
         print("Cedula: ",self.cedula)
 
-asp = aspirante("Juan Perez","1309768932","juan.perez@gmail.com","0987654321","Av. Siempre Falsa 33")
-asp.Registrarse()
-asp.postularse()
-asp.consultarEstado()
+class Administrador(Usuario):
+    def __init__(self, nombre, cedula, correo, ):
+        super().__init__(nombre, cedula, correo)
+
+    def Registrarse(self):
+        print("Se ha registrado")
+        print(f"Nombre: {self.nombre}") 
+        print(f"Cédula: {self.cedula}")
+        print(f"Correo: {self.correo}")
+
+asp1 = Aspirante("Juan Perez","1309768932","juan.perez@gmail.com","0987654321","Av. Siempre Falsa 33")
+admin1 = Administrador("Santiago",1234567899,"santy123@gmail.com")
+
+print("----ASPIRANTE----")
+asp1.Registrarse()
+asp1.postularse()
+asp1.consultarEstado()
+
+print("\n----ADMINISTRADOR----")
+admin1.Registrarse()
