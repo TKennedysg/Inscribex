939061691b4a5be0e881bdf96ba650d8e56833b2
Jorge5084 <karpitejorge@gmail.com>
2025-10-27 04:33:01 -0500

    Actualización de mi--

--- class-Registro.py
+++ class-Registro.py
@@ -1,21 +1,21 @@
 class Registro:
     def __init__(self, idRegistro, correo, estado):
-        print(f"Se ha creado el registro {idRegistro} con correo {correo} y estado {estado}")
-        self.idRegistro = idRegistro
-        self.correo = correo
-        self.estado = estado
+        print(f"Se ha creado el registro {idRegistro} con el correo {correo} y en estado {estado}")
+        self._idRegistro = idRegistro
+        self.__correo = correo
+        self._estado = estado
 
     def registrar(self):
-        print(f"El registro {self.idRegistro} ha sido guardado.")
+        print(f"El registro {self._idRegistro} ha sido guardado.")
 
     def actualizarEstado(self, nuevo_estado):
-        print(f"El registro {self.idRegistro} cambia de {self.estado} a {nuevo_estado}")
-        self.estado = nuevo_estado
+        print(f"El registro {self._idRegistro} cambia de {self._estado} a {nuevo_estado}")
+        self._estado = nuevo_estado
 
     def obtenerDatos(self):
-        print(f"Datos = ID: {self.idRegistro}, Correo: {self.correo}, Estado: {self.estado}")
+        print(f"Datos = ID: {self._idRegistro}, Correo: {self.__correo}, Estado: {self._estado}")
 
-simulacion1 = Registro("411", "karpite@gmail.com", "En proceso")
+simulacion1 = Registro("105", "karpite@gmail.com", "En proceso")
 simulacion1.registrar()
 simulacion1.actualizarEstado("Terminado")
 simulacion1.obtenerDatos()
