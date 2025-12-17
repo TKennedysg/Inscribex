c18b289a91dfdc55968935624f46f87ccd4a7c13
tyronealan <alananchundia27@gmail.com>
2025-12-17 15:52:54 -0500

    correcion de lineas

--- login/observer/notificaciones.py
+++ login/observer/notificaciones.py
@@ -3,4 +3,4 @@ from observer.observer import Observer
 class Notificacion(Observer):
 
     def actualizar(self, mensaje):
-        print(f"📩 Notificación recibida: {mensaje}")
+        print(f"Notificación recibida: {mensaje}")
