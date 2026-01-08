5bfaf7de6482b9920b6520a5c634f55371033883
AdonisMer <merobarcia12@gmail.com>
2026-01-08 16:52:44 -0500

    Restablecimiento rutas

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from routes.datos_personales import datos_personales_bp 
-from routes.usuarios import usuarios_bp
+#from routes.usuarios import usuarios_bp
 from routes.datos_vivienda import vivienda_bp
 from routes.datos_academicos import datos_academicos_bp
 from routes.datos_servicios_basicos import servicios_bp
@@ -16,7 +16,7 @@ app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(datos_personales_bp, url_prefix="/api/python")
-app.register_blueprint(usuarios_bp, url_prefix="/api/python")
+#app.register_blueprint(usuarios_bp, url_prefix="/api/python")
 app.register_blueprint(vivienda_bp, url_prefix="/api/python")
 app.register_blueprint(datos_academicos_bp, url_prefix="/api/python")
 app.register_blueprint(servicios_bp, url_prefix="/api/python")
