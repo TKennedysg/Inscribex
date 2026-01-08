0895f8d41d3fd94c7e2c0e752f9b1176b043c089
Jorge5084 <karpitejorge@gmail.com>
2026-01-08 14:20:06 -0500

    Creacion de CRUD de datos periodo

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from routes.datos_personales import datos_personales_bp
 from routes.datos_vivienda import vivienda_bp
 from routes.datos_academicos import datos_academicos_bp
 from routes.datos_servicios_basicos import servicios_bp
+from routes.datos_periodo import periodos_bp
 
 app = Flask(__name__)
 CORS(app)
@@ -14,6 +15,7 @@ app.register_blueprint(datos_personales_bp, url_prefix="/api/python")
 app.register_blueprint(vivienda_bp, url_prefix="/api/python")
 app.register_blueprint(datos_academicos_bp, url_prefix="/api/python")
 app.register_blueprint(servicios_bp, url_prefix="/api/python")
+app.register_blueprint(periodos_bp, url_prefix="/api/python")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=4000)
